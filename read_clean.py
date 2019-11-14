a73bcda79350fae5ea491611f81476015e1ae4d7
sjmoore3 <54607172+sjmoore3@users.noreply.github.com>
2019-11-13 20:41:57 -0500

    Update read_clean.py
    #Renaming column names to more readable names (line 23)
    #DateTime conversion (line 26)
    fixed #Categorical Variables (lines 29-37)
    added ###BASIC STATISTICS### (lines 53-58)

--- read_clean.py
+++ read_clean.py
@@ -1,9 +1,11 @@
+###IMPORTING DATA###
+
 import pandas as pd
 import numpy as np
 
-file = 'london_merged.tsv'
-df = pd.read_csv(file, sep='\t')
+df = pd.read_csv('london_merged.csv')
 print(df.info())
+print ('-'*80 + '\n')
 
 # "timestamp" - timestamp field for grouping the data
 # "cnt" - the count of a new bike shares
@@ -18,17 +20,21 @@ print(df.info())
 
 
 # Renaming columns names to more readable names
-df.rename(columns={'timestamp':'date_time',
-                        'cnt':'count',
-                        't1':'temp',
-                        't2':'tempf',
-                        'hum':'humidity'},inplace=True)
+df.rename(columns={'timestamp':'Time','cnt':'Count','t1':'Temp','t2':'TempF','hum':'Humidity','wind_speed':'Speed','weather_code':'Weather Code','is_holiday':'Holiday?','is_weekend':'Weekend?','season':'Season'},inplace=True)
+
+# date time conversion
+df['Time'] = pd.to_datetime(df['Time'], format ="%Y-%m-%d %H:%M:%S")
 
 # categorical variables
-df['weather_code'] = df.season.astype('category')
-df['season'] = df.season.astype('category')
-df['is_holiday'] = df.season.astype('category')
-df['is_weekend'] = df.season.astype('category')
+print('Data types before change:')
+print(df.dtypes)
+df['Weather Code'] = df['Weather Code'].astype('category')
+df['Holiday?'] = df['Holiday?'].astype('category')
+df['Weekend?'] = df['Weekend?'].astype('category')
+df['Season'] = df['Season'].astype('category')
+print('\n','Data types after change:')
+print(df.dtypes)
+print ('-'*80 + '\n')
 
 # "weather_code" category description:
 # 1 = Clear ; mostly clear but have some values with haze/fog/patches of fog/fog in vicinity
@@ -44,30 +50,9 @@ df['is_weekend'] = df.season.astype('category')
 # "is_weekend" - boolean field - 1 if the day is weekend
 # "season" - category field meteorological seasons: 0-spring ; 1-summer; 2-fall; 3-winter.
 
-
-# date time conversion
-df['date_time'] = pd.to_datetime(df['date_time'], format ="%Y-%m-%d %H:%M:%S")
-
-# separating month, day, and hour
-df['month'] = df['date_time'].apply(lambda x : str(x).split(' ')[0].split('-')[1])
-df['day'] = df['date_time'].apply(lambda x : str(x).split(' ')[0].split('-')[2])
-df['hour'] = df['date_time'].apply(lambda x : str(x).split(' ')[1].split(':')[0])
-
-
-
-# boxplots of bikeshare use by month, day, and hour
-sns.boxplot(data=df, x='month', y='count')
-plt.show()
-sns.boxplot(data=df, x='day', y='count')
-plt.show()
-sns.boxplot(data=df, x='hour', y='count')
-plt.show()
-
-# barplots of total use on weekend, season, and holiday
-sns.barplot(x=df['is_weekend'],y=df['count'])
-plt.show()
-sns.barplot(x=df['is_holiday'],y=df['count'])
-plt.show()
-sns.barplot(x=df['season'],y=df['count'])
-plt.show()
-
+###BASIC STATISTICS###
+print('Are there any missing values?:')
+print(df.isnull().values.any())
+print ('-'*80 + '\n')
+print(df.describe())
+print ('-'*80 + '\n')
